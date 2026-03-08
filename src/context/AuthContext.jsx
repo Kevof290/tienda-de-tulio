626d4bd6a60ee5c569a70ad9f9028111836721e0
Kevin Alfaro <kevin.alfaro290@gmail.com>
2026-03-08 16:41:04 -0300

    se corrige sintaxis para correcta visualizacion

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -30,8 +30,10 @@ export const AuthProvider = ({children}) => {
     }
 
     return (
-        <AuthContext.Provider value={{user, token, login, logout}}>children</AuthContext.Provider>
-    )
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export const useAuth = () => useContext(AuthContext)
